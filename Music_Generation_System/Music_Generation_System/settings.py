d5938d69bee5b3f5775300e8570cac4661b9d748
Laxman Maharjan <lxmnmrzn@gmail.com>
2022-02-02 13:30:16 +0545

    adding STATICROOT

--- Music_Generation_System/Music_Generation_System/settings.py
+++ Music_Generation_System/Music_Generation_System/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
